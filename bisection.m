8951d2796823243ad5984711658f2b4ce04f6e0e
KienDang1101 <kientrung40021101@gmail.com>
2024-12-11 22:51:59 +0700

    Update bisection.m

--- bisection.m
+++ bisection.m
@@ -2,11 +2,13 @@ function [x0, n, er] = bisection(fx, a, b, e)
     n = 0;
     x0 = 0;
     fx = str2func(['@(x)', fx]);
+
     %Condition to use bisection method
     if fx(a) * fx(b) >= 0
         er = 'Check your input! fx(a) * fx(b) must be < 0';
         return;
     end
+
     %Bisection method
     while(abs(a-b) >= e)
         n = n + 1;
