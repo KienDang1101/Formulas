68d11f1aab50eac09975d30efb03daefff31af58
kien172 <91733054+kien172@users.noreply.github.com>
2024-12-11 23:09:47 +0700

    Add files via upload

---
+++ newtonInterpolation.m
@@ -0,0 +1,19 @@
+function [P, result] = newtonInterpolation(xa, ya, x0)
+    %da
+    n = length(xa);
+    d = ya;
+    for i = 1 : n
+        for j = 1 : i - 1
+            d(i) = (d(j) - d(i)) / (xa(j) - xa(i));
+        end
+    end
+    %result
+    syms X;
+    n = length(d);
+    result = d(n);
+    P = d(n);
+    for i = (n - 1) : -1 : 1
+        result = result * (x0 - xa(i)) + d(i);
+        P = P * (X - xa(i)) + d(i);
+    end
+end
