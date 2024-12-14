0f6e50ca06ad2d6cbe0a75f3e6c5338dede64be3
kien172 <91733054+kien172@users.noreply.github.com>
2024-12-14 15:54:36 +0700

    Update EulerExp.m

--- EulerExp.m
+++ EulerExp.m
@@ -1,25 +1,8 @@
 function [a,b] = EulerExp(x,y)
-x0=x;
-y0=log(y);
-n=length(x0);
-sumx=0;
-sumy=0;
-sumxy=0;
-sumx2=0;
-st=0;
-sr=0;
-for i=1:n
-    sumx=sumx+x0(i);
-    sumy=sumy+y0(i);
-    sumxy=sumxy+x0(i)*y0(i);
-    sumx2=sumx2+x0(i)*x0(i);
-end
+xi = x;
+yi = log(y);
+[b, a] = Regress(xi,yi)
 
-xm=sumx/n;
-ym=sumy/n;
-b=(n*sumxy - sumx*sumy)/(n*sumx2 - sumx*sumx);
-a=ym-b*xm;
-
-a=exp(a);
-b=b;
+a = exp(a);
+b = b;
 end
