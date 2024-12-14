397893896ee541f01a7f9f38be443d41c4deea3e
kien172 <91733054+kien172@users.noreply.github.com>
2024-12-14 15:42:40 +0700

    EulerExp.m

--- EulerExp.m
+++ EulerExp.m
@@ -1,6 +1,6 @@
 function [a,b] = EulerExp(x,y)
 x0=x;
-y0=log10(y);
+y0=log(y);
 n=length(x0);
 sumx=0;
 sumy=0;
@@ -14,14 +14,12 @@ for i=1:n
     sumxy=sumxy+x0(i)*y0(i);
     sumx2=sumx2+x0(i)*x0(i);
 end
+
 xm=sumx/n;
 ym=sumy/n;
 b=(n*sumxy - sumx*sumy)/(n*sumx2 - sumx*sumx);
 a=ym-b*xm;
-for i=1:n
-    st=st+pow2(y0(i)-ym);
-    sr=sr + pow2(y0(i)-b*x0(i) -a);
-end
+
 a=exp(a);
 b=b;
 end
