3a18c5cd5f0876a4702bb5da9d49b070c8d3a74c
topstrong89 <92913823+topstrong89@users.noreply.github.com>
2024-12-14 15:20:37 +0700

    Add files via upload

---
+++ Regress.m
@@ -0,0 +1,19 @@
+function [a,b] = Regress(x,y)
+n=length(x);
+sumx=0;
+sumy=0;
+sumxy=0;
+sumx2=0;
+st=0;
+sr=0;
+for i=1:n
+    sumx=sumx+x(i);
+    sumy=sumy+y(i);
+    sumxy=sumxy+x(i)*y(i);
+    sumx2=sumx2+x(i)*x(i);
+end
+xm=sumx/n;
+ym=sumy/n;
+a=(n*sumxy - sumx*sumy)/(n*sumx2 - sumx*sumx);
+b=ym-a*xm;
+end
