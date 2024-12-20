38c5c1868b757028a4bf62ce0398779f94a4ac68
kien172 <91733054+kien172@users.noreply.github.com>
2024-12-20 18:27:03 +0700

    Update readArray.m

--- readArray.m
+++ readArray.m
@@ -1,3 +1,17 @@
-function [A] = readArray(S)
-    
+function [A] = readArray(str)
+    % IF S IS A EQUATION
+    findX = 0 + double(strfind(str, 'x'));
+    if  findX ~= 0
+        % f(x)
+        A = str2sym(str);
+    else
+        % IF NOT A EQUATION
+        if 0 + double(strfind(str, ':')) ~= 0
+            A = eval(str);
+        else
+            str = regexprep(str, '[[]]', '');
+            str = regexprep(str, ',', ' ');
+            A = str2double(strsplit(str));
+        end
+    end
 end
