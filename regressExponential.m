a7f331d3fd22b0f0ab9c9db89eca151719095016
KienDang1101 <kientrung40021101@gmail.com>
2024-12-20 17:44:53 +0700

    regressExponential.m

--- regressExponential.m
+++ regressExponential.m
@@ -3,7 +3,7 @@ function [a,b] = regressExponential(x, y)
     x = log10(x);
     y = log10(y);
 
-    [B, A] = RegressLinear(x, y);
+    [B, A] = regressLinear(x, y);
     a = 10^A;
     b = B;
 end
